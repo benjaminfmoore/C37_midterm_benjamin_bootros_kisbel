a8db86c027d23ac535fa070d19286655846804bc
kisbel <kisbel46@gmail.com>
2020-05-18 15:42:54 -0400

    added footer, fixed search, added/edited CSS

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import SearchForm from './components/SearchForm';
 import { Container } from 'react-bootstrap';
-
+import Footer from './components/footer';
 import './App.css';
 
 const App = () => {
   return (
+    <>
+    <h1>LYRICALLY</h1>
     <Container>
       <SearchForm />
     </Container>
+    <Footer />
+    </>
   );
 };
+
 
 export default App;
