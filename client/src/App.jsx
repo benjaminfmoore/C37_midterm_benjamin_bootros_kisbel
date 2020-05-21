2aad4122aa417151e203b21e1363d266bf752db4
benjaminfmoore <benjaminfmoore@yahoo.com>
2020-05-20 20:52:30 -0400

    Edited the reactDOM and made navbar buttons work

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import SearchForm from './components/SearchForm';
 import { Container } from 'react-bootstrap';
 import Footer from './components/footer';
 import NavBar from './components/NavBar';
 import './App.css';
 import './index.css';
+import AboutUs from './components/AboutUs';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <NavBar />
-      <h1>LYRICALLY</h1>
-      <Container>
-        <SearchForm />
-      </Container>
+      <Switch>
+        <Route exact path="/">
+          <Container>
+            <h1>LYRICALLY</h1>
+            <SearchForm />
+          </Container>
+        </Route>
+        <Route exact path="/about" component={AboutUs} />
+      </Switch>
       <Footer />
-    </>
+    </Router>
   );
 };
 
