bbe369ed61c0cf3a36ed09d189e1b7c3e15a25d2
kisbel <kisbel46@gmail.com>
2020-05-20 21:07:27 -0400

    toggle theme working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SearchForm from './components/SearchForm';
 import { Container } from 'react-bootstrap';
 import Footer from './components/footer';
 import NavBar from './components/NavBar';
 import './App.css';
 import './index.css';
+import CassetteBackground from './pictures/cassette.png';
+import CDBackground from './pictures/cd-png-10.png';
+import RecordBackground from './pictures/record.png';
+import GreyBackground from './pictures/grey.png'
+
 
 const App = () => {
+  const [backgroundImg, setBackgroundImg] = useState('');
+
+  const handleBackgroundChange = newBackground => {
+    if (newBackground === 'cassette') setBackgroundImg(CassetteBackground)
+    else if (newBackground === 'cd') setBackgroundImg(CDBackground)
+    else if (newBackground === 'record') setBackgroundImg(RecordBackground)
+    else if (newBackground === 'grey') setBackgroundImg(GreyBackground)
+  }
+
+  
   return (
-    <>
+    <div className="app-container" style={{ backgroundImage: `url(${backgroundImg})` }}>
       <NavBar />
       <h1>LYRICALLY</h1>
       <Container>
         <SearchForm />
       </Container>
-      <Footer />
-    </>
+      <Footer handleBackgroundChange={handleBackgroundChange} />
+    </div>
   );
 };
 
--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -1,27 +1,42 @@
 import React from 'react';
 import cd from '../pictures/cd-png-10.png';
-import record from '../pictures/record PNG.png';
+import record from '../pictures/record.png';
 import cassette from '../pictures/cassette.png';
+import grey from '../pictures/grey.png'
+import '../index.css';
 
-function Footer() {
+
+function Footer(props) {
+
+
   return (
+    <>
+
     <footer className="site-footer">
+
       <div id="createdBy">
-        <p>Created by Kisbel and Benjamin at Wyncode!</p>
+        <p>Created by Kisbel and Benjamin at Wyncode</p>
       </div>
-      <a href="" id="recordButton">
+
+      <button id="recordButton" onClick={() => props.handleBackgroundChange('record')}>
         <img src={record} width="40" height="40" />
-      </a>
+      </button>
 
-      <a href="" id="cassetteButton">
+      <button id="cassetteButton" onClick={() => props.handleBackgroundChange('cassette')}>
         <img src={cassette} width="40" height="40" />
-      </a>
+      </button>
 
-      <a href="" id="cdButton">
+      <button id="cdButton" onClick={() => props.handleBackgroundChange('cd')}>
         <img src={cd} width="40" height="40" />
-      </a>
+      </button>
+
+      <button id="greyButton" onClick={() => props.handleBackgroundChange('grey')}>
+        <img src={grey} width="40" height="40" />
+      </button>
     </footer>
+    </>
   );
+ 
 }
 
 export default Footer;
